8dc772d86122b2c6ea829f7b4546d38c3f888828
Vijay Kumar <contactvkr@yahoo.com>
2022-12-05 20:21:42 +0530

    Create config.php

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+error_reporting(0);
+$serverDomain = "http://theofficialvkr.ml";
+$myDomain = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]";
+$dlAPI = ""; //Downloader Api Key
+$searchAPI = ""; //Search Api Key
+?>
