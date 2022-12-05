7136041c18d6c442a2a0da021c76b9de519b2957
Vijay Kumar <contactvkr@yahoo.com>
2022-12-05 20:20:52 +0530

    Delete config.php

--- config.php
+++
@@ -1,7 +0,0 @@
-<?php
-error_reporting(0);
-$serverDomain = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]";
-$myDomain = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]/demo";
-$dlAPI = "VKrFork"; //Downloader Api Key
-$searchAPI = "VKrFork"; //Search Api Key
-?>
