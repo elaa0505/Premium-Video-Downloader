c84e8db9a94eba7733dc6d324a664fa4d05f88be
Vijay Kumar <contactvkr@yahoo.com>
2022-12-06 16:00:31 +0530

    Update

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 error_reporting(0);
-$serverDomain = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]";
+$serverDomain = "http://vkrfork.ml";
 $myDomain = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]";
 $dlAPI = ""; //Downloader Api Key
 $searchAPI = ""; //Search Api Key
